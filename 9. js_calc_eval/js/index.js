98400ba2cad546771f05bd8acd701b9cad999238
Roman Kuzmenko <83546337+KuzmenkoRomanJS@users.noreply.github.com>
2022-02-10 23:07:17 +0200

    Uplad project
    Ыome bugs and with necessary troubleshooting

---
+++ 9. js_calc_eval/js/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const calc = document.querySelector(".calc");
+const result = document.querySelector("#result");
+
+calc.addEventListener("click", function (event) {
+  if (!event.target.classList.contains("calc__btn")) return;
+
+  const value = event.target.innerText;
+
+  switch (value) {
+    case "C":
+      result.innerText = "";
+      break;
+
+    case "=":
+      if (result.innerText.search(/[^0-9*/+-.]/im) != -1) return;
+      result.innerText = eval(result.innerText).toFixed(2);
+      break;
+
+    default:
+      result.innerText += value;
+  }
+});
